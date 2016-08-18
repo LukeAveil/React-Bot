47eee3ebde69f711fe4d815ee473b58174c554e9
Luke <lukeaveil@yahoo.com>
2016-08-18 12:27:54 +0100

    adds inbox item and inbox pane table

--- 02_ReactIntroduction/Code/start/app/app.js
+++ 02_ReactIntroduction/Code/start/app/app.js
@@ -7,8 +7,47 @@ var App = React.createClass({
       <div>
         <div id="header"></div>
         <div className="container">
+          <div className="column">
+            <InboxPane />
+          </div>
+          <div className="column"></div>
+          <div className="column"></div>
         </div>
       </div>
+    )
+  }
+})
+
+var InboxPane = React.createClass({
+  render: function(){
+    return (
+      <div id="inbox-pane">
+        <h1>Inbox</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Chat Received</th>
+              <th>Name</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <InboxItem />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+})
+
+var InboxItem = React.createClass({
+  render: function(){
+    return (
+      <tr>
+        <td>5pm</td>
+        <td>I love pizza</td>
+        <td>Confirmed</td>
+      </tr>
     )
   }
 })
